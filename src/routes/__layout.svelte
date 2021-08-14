eb5053f9bb9f865f1ef085cbf7c439eea4cdf9b5
unknown <ronaldvm@gmail.com>
2021-08-14 20:23:58 +0200

    Final Version

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  // import "../app.postcss";
+  import '../app.css';
+  import Nav from '$lib/nav.svelte'
+  </script>
+  
+  <Nav />
+
+  <div class="container max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+    <slot />
+  </div>
