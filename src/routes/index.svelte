e525ebfaf87da5bfac9205e0d0357ba2b21be462
unknown <ronaldvm@gmail.com>
2021-08-14 21:06:22 +0200

    Tekst aangepast

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -39,22 +39,22 @@
     export let posts
   </script>
   
-  <h1 class="text-4xl font-semibold mb-7 text-gray-700">GraphCMS!! starter blog!</h1>
-  <ul>
-    <li>
-      {#each posts as post}
-      <div class="card text-center shadow-xl border mb-10">
-        <figure class="px-10 pt-10"></figure>
-          <img src="{post.coverImage.url}" alt="{post.coverImage.fileName}" class="rounded-xl" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">{post.title}</h2>
-          <p class="prose-lg">{post.excerpt}</p>
-          <div class="justify-end card-actions">
-            <a href="post/{post.slug}" class="btn btn-secondary">➜ {post.title}</a>
-          </div>
+<h1 class="text-4xl font-semibold mb-7 text-gray-700">GraphCMS starter blog</h1>
+<ul>
+  <li>
+    {#each posts as post}
+    <div class="card text-center shadow-xl border mb-10">
+      <figure class="px-10 pt-10">
+        <img src="{post.coverImage.url}" alt="{post.coverImage.fileName}" class="rounded-xl" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{post.title}</h2>
+        <p class="prose-lg">{post.excerpt}</p>
+        <div class="justify-end card-actions">
+          <a href="post/{post.slug}" class="btn btn-secondary">➜ {post.title}</a>
         </div>
       </div>
-      {/each}
-    </li>
-  </ul>
+    </div>
+    {/each}
+  </li>
+</ul>
